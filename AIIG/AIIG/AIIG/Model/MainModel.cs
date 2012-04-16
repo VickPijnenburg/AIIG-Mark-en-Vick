63a0c3de7e5c25b44430db3e0277184dcf9e6f40
Vick Pijnenburg <vp@home.nl>
2012-04-16 09:46:00 +0200

    Made first classes

---
+++ AIIG/AIIG/AIIG/Model/MainModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIIG.Model
+{
+    class MainModel
+    {
+        private static MainModel instance;
+
+        private MainModel()
+        {
+            instance = this;
+        }
+
+        public static MainModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    new MainModel();
+                }
+
+                return instance;
+            }
+        }
+    }
+}

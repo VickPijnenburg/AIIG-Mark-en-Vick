a3c5b15d260a1a767543ca656457c076f0fceeb7
Vick Pijnenburg <vp@home.nl>
2012-05-07 10:21:48 +0200

    draw in Entiteit aangemaakt

--- AIIG/AIIG/AIIG/Model/Entiteit.cs
+++ AIIG/AIIG/AIIG/Model/Entiteit.cs
@@ -2,11 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using AIIG.View;
 
 namespace AIIG.Model
 {
 	class Entiteit
 	{
-		private Node positie;
+
+        //Fields
+
+		private Node node;
+        private Texture2D texture;
+
+
+
+        //Constructors
+
+        public Entiteit(Texture2D startTexture)
+        {
+            texture = startTexture;
+        }
+
+
+
+        //Properties
+
+        public Texture2D Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
+
+
+
+        //Methods
+
+        public void Draw(GameTime gameTime)
+        {
+            MainView.Instance.SpriteBatch.Draw(texture, node.Position, Color.White);
+        }
 	}
 }

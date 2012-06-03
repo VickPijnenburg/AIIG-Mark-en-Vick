402004a7f2ace189a65e2d7d3b18b498e5716426
Vick Pijnenburg <vp@home.nl>
2012-06-03 21:19:27 +0200

    Renamed Game1 to MainGame, made it a singleton

--- AIIG/AIIG4/AIIG4/MainGame.cs
+++ AIIG/AIIG4/AIIG4/MainGame.cs
@@ -14,34 +14,61 @@ namespace AIIG4
 	/// <summary>
 	/// This is the main type for your game
 	/// </summary>
-	public class Game1 : Microsoft.Xna.Framework.Game
+	public class MainGame : Microsoft.Xna.Framework.Game
 	{
+
+        //////////////////////////////
+        //Fields//
+        //////////////////////////////
+
+        private static MainGame instance;
+
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 
-		public Game1()
+
+
+        //////////////////////////////
+        //Constructors//
+        //////////////////////////////
+
+		private MainGame()
 		{
+            instance = this;
+
 			graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
 		}
 
-		/// <summary>
-		/// Allows the game to perform any initialization it needs to before starting to run.
-		/// This is where it can query for any required services and load any non-graphic
-		/// related content.  Calling base.Initialize will enumerate through any components
-		/// and initialize them as well.
-		/// </summary>
+
+
+        //////////////////////////////
+        //Properties//
+        //////////////////////////////
+
+        public static MainGame Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    new MainGame();
+                }
+                return instance;
+            }
+        }
+
+
+
+        //////////////////////////////
+        //Methods//
+        //////////////////////////////
+
 		protected override void Initialize()
 		{
-			// TODO: Add your initialization logic here
-
 			base.Initialize();
 		}
 
-		/// <summary>
-		/// LoadContent will be called once per game and is the place to load
-		/// all of your content.
-		/// </summary>
 		protected override void LoadContent()
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
@@ -50,20 +77,11 @@ namespace AIIG4
 			// TODO: use this.Content to load your game content here
 		}
 
-		/// <summary>
-		/// UnloadContent will be called once per game and is the place to unload
-		/// all content.
-		/// </summary>
 		protected override void UnloadContent()
 		{
 			// TODO: Unload any non ContentManager content here
 		}
 
-		/// <summary>
-		/// Allows the game to run logic such as updating the world,
-		/// checking for collisions, gathering input, and playing audio.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update(GameTime gameTime)
 		{
 			// Allows the game to exit
@@ -75,10 +93,6 @@ namespace AIIG4
 			base.Update(gameTime);
 		}
 
-		/// <summary>
-		/// This is called when the game should draw itself.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Draw(GameTime gameTime)
 		{
 			GraphicsDevice.Clear(Color.CornflowerBlue);

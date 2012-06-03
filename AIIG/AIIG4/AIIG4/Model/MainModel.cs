5f621b9e61e4e1929f8e00cd8989933aee431d72
Vick Pijnenburg <vp@home.nl>
2012-06-03 21:26:20 +0200

    MainModel en MainView aangemaakt

---
+++ AIIG/AIIG4/AIIG4/Model/MainModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIIG4.Model
+{
+    public class MainModel
+    {
+
+        //////////////////////////////
+        //Properties//
+        //////////////////////////////
+
+        private static MainModel instance;
+
+
+
+        //////////////////////////////
+        //Constructors//
+        //////////////////////////////
+
+        private MainModel()
+        {
+            instance = this;
+        }
+
+
+
+        //////////////////////////////
+        //Properties//
+        //////////////////////////////
+
+        public static MainModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    new MainModel();
+                }
+                return instance;
+            }
+        }
+    }
+}
---
+++ AIIG/AIIG4/AIIG4/View/MainView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIIG4.View
+{
+    public class MainView
+    {
+
+        //////////////////////////////
+        //Properties//
+        //////////////////////////////
+
+        private static MainView instance;
+
+
+
+        //////////////////////////////
+        //Constructors//
+        //////////////////////////////
+
+        private MainView()
+        {
+            instance = this;
+        }
+
+
+
+        //////////////////////////////
+        //Properties//
+        //////////////////////////////
+
+        public static MainView Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    new MainView();
+                }
+                return instance;
+            }
+        }
+}

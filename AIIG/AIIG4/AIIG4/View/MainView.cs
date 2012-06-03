79e4de437e122c88c09ebe190e93ef30896a59b6
Mark Bos <markiebosie@hotmail.com>
2012-06-03 22:02:07 +0200

    zooi gefixt

--- AIIG/AIIG4/AIIG4/View/MainView.cs
+++ AIIG/AIIG4/AIIG4/View/MainView.cs
@@ -5,41 +5,42 @@ using System.Text;
 
 namespace AIIG4.View
 {
-    public class MainView
-    {
+	public class MainView
+	{
 
-        //////////////////////////////
-        //Properties//
-        //////////////////////////////
+		//////////////////////////////
+		//Properties//
+		//////////////////////////////
 
-        private static MainView instance;
-
-
-
-        //////////////////////////////
-        //Constructors//
-        //////////////////////////////
-
-        private MainView()
-        {
-            instance = this;
-        }
+		private static MainView instance;
 
 
 
-        //////////////////////////////
-        //Properties//
-        //////////////////////////////
+		//////////////////////////////
+		//Constructors//
+		//////////////////////////////
 
-        public static MainView Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    new MainView();
-                }
-                return instance;
-            }
-        }
+		private MainView()
+		{
+			instance = this;
+		}
+
+
+
+		//////////////////////////////
+		//Properties//
+		//////////////////////////////
+
+		public static MainView Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					new MainView();
+				}
+				return instance;
+			}
+		}
+	}
 }

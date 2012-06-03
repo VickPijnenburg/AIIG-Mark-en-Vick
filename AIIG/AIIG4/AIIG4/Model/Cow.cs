5d9e01ca0aa78be3e2f3dc66a5348cff2f6dc134
Mark Bos <markiebosie@hotmail.com>
2012-06-03 21:36:03 +0200

    entity zooi begin

---
+++ AIIG/AIIG4/AIIG4/Model/Cow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AIIG4.Model
+{
+	class Cow : Entity
+	{
+
+		//////////////////////////////
+		//Constructors//
+		//////////////////////////////
+
+		public Cow(Texture2D startTexture)
+			:base(startTexture)
+		{
+
+		}
+	}
+}
---
+++ AIIG/AIIG4/AIIG4/Model/Entity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AIIG4.Model
+{
+	class Entity
+	{
+
+        //////////////////////////////
+        //Fields//
+        //////////////////////////////
+
+        private Texture2D texture;
+
+
+		//////////////////////////////
+		//Constructors//
+		//////////////////////////////
+
+		public Entity(Texture2D startTexture)
+		{
+            this.texture = startTexture;
+		}
+	}
+}
